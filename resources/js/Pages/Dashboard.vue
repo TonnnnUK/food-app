103d34ac374fa0faafd453f28642a983cc122bf6
Anton Hutchinson <a.hutchinson86@gmail.com>
2023-07-05 16:42:53 +0100

    Optimise dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,40 +6,48 @@
             <h2 class="flex items-end font-semibold leading-tight text-gray-800 md:text-xl">Welcome, <span class="ml-2 text-sm">{{$page.props.auth.user.username}}</span> </h2>
         </template>
 
-        <div class="py-12">
+        <div class="py-4 lg:py-8">
 
-            <div class="mx-auto mb-4 max-w-7xl sm:px-6 lg:px-8">
+            <div class="mx-auto mb-4 w-[95%] max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white rounded-lg shadow-sm ">
                     <h2 class="p-6">What's goin on, then?</h2>
 
                     <div class="p-6 mb-4 ">
                         <h3 class="text-lg">Meals</h3>
 
-                        <table v-for="entry of entries" :key="entry.id" class="text-xs border-b dark:border-neutral-500" >
-                            <tr v-if="entry.entry_type == 'Meal' || entry.entry_type == 'Recipe'">
-                                <td class="w-40 px-2 py-4">
-                                    <span :class="entry.in_past ? 'px-2 py-1 text-red-600 font-bold' : 'px-2 py-1' ">{{ entry.date_time_formatted }}</span> 
-                                </td>
-                                <td class="px-2 py-4 w-28">{{ entry.date_time_human }} </td>
-                                <td class="px-2 py-4 font-bold w-60">
+                        <div v-for="entry of entries" :key="entry.id" class="text-xs border-b dark:border-neutral-500">
+                            <div class="flex flex-wrap py-4 md:py-0 md:flex-row" v-if="entry.entry_type == 'Meal' || entry.entry_type == 'Recipe'">
+                                <div class="w-full md:py-4 md:w-40">
+                                    <span :class="entry.in_past ? 'text-red-600 font-bold' : '' ">{{ entry.date_time_formatted }}</span> 
+                                </div>
+                                <div class="w-full italic md:py-4 md:w-28">{{ entry.date_time_human }} </div>
+                                <div class="w-full my-4 font-bold md:py-4 md:w-60 md:my-0">
                                     <span class="capitalize">{{ entry.meal ? entry.meal.name : entry.recipe.name }}</span>
-                                </td>
-                                <td class="flex gap-1 px-2 py-4">
-                                    <span class="px-2 py-1 mx-1 transition duration-200 bg-green-100 border border-green-200 rounded-full cursor-pointer hover:bg-green-200"
-                                        v-on:click="data.handleComplete = entry.id; data.handleLeftovers = 0;"
-                                    >
-                                        Finished
-                                    </span>
-                                    <span class="px-2 py-1 mx-1 transition duration-200 bg-yellow-200 border border-yellow-300 rounded-full cursor-pointer hover:bg-yellow-300"
-                                        v-on:click="data.handlingLeftovers = entry.id; data.handleComplete = 0"
-                                    >
-                                        Leftovers?
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr v-if="data.handlingLeftovers == entry.id">
-                                <td class="p-2 my-2" colspan="4">
-                                    <div class="flex items-center gap-2">
+                                </div>
+                                <div class="flex items-center gap-1 md:py-4">
+                                    <div>
+                                        <span class="px-2 py-1 mx-1 transition duration-200 bg-green-100 border border-green-200 rounded-full cursor-pointer select-none hover:bg-green-200"
+                                            v-on:click="data.handleComplete = entry.id; data.handlingLeftovers = 0;"
+                                        >
+                                            Finished
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <span class="px-2 py-1 mx-1 transition duration-200 bg-yellow-200 border border-yellow-300 rounded-full cursor-pointer select-none hover:bg-yellow-300"
+                                            v-on:click="data.handlingLeftovers = entry.id; data.handleComplete = 0"
+                                        >
+                                            Leftovers?
+                                        </span>
+                                    </div>
+                                    <div class="px-2 py-1 text-lg cursor-pointer" v-if="data.handlingLeftovers == entry.id || data.handleComplete == entry.id" 
+                                        v-on:click="data.handlingLeftovers = 0; data.handleComplete = 0;">
+                                        x
+                                    </div>
+                                </div>
+                            </div>
+                            <div v-if="data.handlingLeftovers == entry.id">
+                                <div class="p-2 my-2" colspan="4">
+                                    <div class="flex flex-col gap-2 sm:flex-row sm:items-center">
                                         <span>Enter leftovers portions</span>
                                         <TextInput class="p-2 text-xs border border-gray-300" v-model="data.leftovers.qty" />
                                         <SelectInput class="text-xs" v-model="data.leftovers.location">
@@ -51,14 +59,14 @@
                                             Save
                                         </button>   
                                     </div>
-                                </td>
-                            </tr>
-                            <tr v-if="data.handleComplete == entry.id">
-                                <td class="p-2" colspan="4">
+                                </div>
+                            </div>
+                            <div v-if="data.handleComplete == entry.id">
+                                <div class="p-2">
                                     Remove from inventory: 
 
                                     <div class="flex flex-wrap my-2 gap-x-2">
-                                        <label v-for="item of entry.meal != null ? entry.meal.ingredients : entry.recipe.ingredients" :key="item.id" class="w-1/4 p-1 py-2 mx-2 cursor-pointer hover:bg-gray-200">
+                                        <label v-for="item of entry.meal != null ? entry.meal.ingredients : entry.recipe.ingredients" :key="item.id" class="w-full p-1 py-2 mx-2 cursor-pointer md:w-1/4 hover:bg-gray-200">
                                             <input type="checkbox" v-model="data.itemsToRemove" :id="item.id" :value="item.id" class="mr-1">
                                             {{ item.name }}
                                         </label>
@@ -67,36 +75,36 @@
                                     <button class="px-3 py-2 mt-2 text-white bg-green-500 border border-green-600 rounded-lg hover:bg-green-600"
                                         v-on:click="completeMeal(entry);"
                                     >Remove Items &amp; Complete</button>   
-                                </td>
-                            </tr>
-                        </table>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
                     <div class="p-6">
                         <h3 class="text-lg">Workouts</h3>
                         
-                         <table v-for="entry of entries" :key="entry.id" class="text-xs border-b dark:border-neutral-500" >
-                            <tr v-if="entry.entry_type == 'Workout' || entry.entry_type == 'Custom Workout'">
-                                <td class="w-40 px-2 py-4">
-                                    <span :class="entry.in_past ? 'px-2 py-1 text-red-600 font-bold' : 'px-2 py-1' ">{{ entry.date_time_formatted }}</span>
-                                </td>
-                                <td class="px-2 py-4 w-28">{{ entry.date_time_human }} </td>
-                                <td class="px-2 py-4 font-bold w-60">
+                         <div v-for="entry of entries" :key="entry.id" class="text-xs border-b dark:border-neutral-500" >
+                            <div class="flex flex-wrap py-4 md:py-0 md:flex-row" v-if="entry.entry_type == 'Workout' || entry.entry_type == 'Custom Workout'">
+                                <div class="w-full md:py-4 md:w-40">
+                                    <span :class="entry.in_past ? 'text-red-600 font-bold' : '' ">{{ entry.date_time_formatted }}</span>
+                                </div>
+                                <div class="w-full italic md:py-4 md:w-28">{{ entry.date_time_human }} </div>
+                                <div class="w-full my-4 font-bold md:py-4 md:w-60 md:my-0">
                                     <span class="capitalize">{{ entry.workout ? entry.workout.name : entry.custom_workout.name }}</span>
-                                </td>
-                                <td class="px-2 py-4">
+                                </div>
+                                <div class="flex items-center gap-1 md:py-4">
                                     <span class="px-2 py-1 bg-green-100 border border-green-200 rounded-full">Completed?</span>
-                                </td>
-                            </tr>
-                        </table>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
                     <div class="p-6">
-                        <h3 class="text-lg">Shopping List</h3>
+                        <h3 class="mb-2 text-lg md:mb-4">Shopping List</h3>
 
-                        <div class="flex flex-col">
+                        <div class="flex flex-col capitalize">
                             <div v-for="item of shopping_list" :key="item.id">
                                 {{ item.name }}
                             </div>
